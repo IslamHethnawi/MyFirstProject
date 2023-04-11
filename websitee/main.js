46648179353ce1fd3b7ad28a38b3eec69d9b1fa1
IslamHethnawi <130461536+IslamHethnawi@users.noreply.github.com>
2023-04-11 15:02:00 +0300

    first project

---
+++ websitee/main.js
@@ -0,0 +1,40 @@
+let countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();// will return time from history start to current time in millSecconds
+// console.log(countDownDate)
+
+let counter = setInterval(() => {
+    // Get Date Now
+    let dateNow = new Date().getTime();
+    // Find The Difference Between now and count down date
+    let dateDiff = countDownDate - dateNow;
+    // Get Time Units
+    // let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    // let hours = Math.floor(dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+    // let minutes = Math.floor(dateDiff % (1000 * 60 * 60)) / (1000 * 60);
+    // let seconds = Math.floor(dateDiff % (1000 * 60)) / 1000;
+
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(dateDiff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(dateDiff % (1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(dateDiff % (1000 * 60) / 1000);
+
+
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days }` : days;
+
+    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+    
+    if (dateDiff <= 0) {
+        clearInterval(counter);
+    }
+}, 1000);
+
+
+
+
+
+
+
